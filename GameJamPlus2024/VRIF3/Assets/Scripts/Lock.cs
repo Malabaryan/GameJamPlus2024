11da41f00c2b95ed7688ff18ec86ce286562e119
Daar375 <Daar375@gmail.com>
2024-10-20 09:12:42 -0700

    Fixed teleport, lock player and arrow collision

---
+++ GameJamPlus2024/VRIF3/Assets/Scripts/Lock.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public class Lock : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float startPostion = 0;
+    void Start()
+    {
+        startPostion = transform.position.y;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, startPostion, transform.position.z);
+    }
+}
