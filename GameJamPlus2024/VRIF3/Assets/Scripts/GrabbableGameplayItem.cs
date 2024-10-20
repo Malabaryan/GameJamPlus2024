1754c16ab4ab02dbc5f4bd88e6012a2e37c30b4f
Leo Lizano <leoln123@live.com>
2024-10-20 10:41:39 -0600

    Updated ship prefab to work properly with grabbable objects

---
+++ GameJamPlus2024/VRIF3/Assets/Scripts/GrabbableGameplayItem.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using BNG;
+
+public class GrabbableGameplayItem : MonoBehaviour
+{
+    [SerializeField] private float heldTimer = 20f;
+    [SerializeField] private SnapZone snapZone;
+
+    private Grabbable grabbableRef;
+    float currentHoldTime = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        grabbableRef = GetComponent<Grabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabbableRef.BeingHeld || snapZone.HeldItem != null)
+        {
+            currentHoldTime = 0f;
+            return;
+        }
+
+        currentHoldTime += Time.deltaTime;
+
+        if(currentHoldTime > heldTimer)
+        {
+            currentHoldTime = 0f;
+            if (snapZone != null)
+                snapZone.GrabGrabbable(grabbableRef);
+                //snapZone.HeldItem = grabbableRef;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.name);
+        if (other.transform.CompareTag("GrabbableTP") && !grabbableRef.BeingHeld)
+        {
+            if(snapZone != null)
+                snapZone.GrabGrabbable(grabbableRef);
+            //snapZone.HeldItem = grabbableRef;
+        }
+    }
+}
