9a0b284ca938e50786938379df862e6e117dc6b8
Daniel Alvarez <Daar375@gmail.com>
2024-11-20 17:31:18 -0600

    PC Emulator movement  fixes

---
+++ GameJamPlus2024/VRIF3/Assets/Scripts/PCEmulator/EmulatorMode.cs
@@ -0,0 +1,32 @@
+using BNG;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EmulatorMode : MonoBehaviour
+{
+    public UnityEvent EmulatorStartEvents;
+    private VREmulator VREmulator;
+    private bool modeSwitch;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        VREmulator = GetComponent<VREmulator>();
+        modeSwitch = VREmulator.HMDIsActive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VREmulator.HMDIsActive != modeSwitch)
+        {
+            if (!VREmulator.HMDIsActive)
+            {
+                EmulatorStartEvents.Invoke();
+            }
+            
+            
+            modeSwitch = VREmulator.HMDIsActive;
+
+        }
+    }
+}
