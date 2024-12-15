bd31a29fece6f6687c68710476fed58858a1e922
Leonardo Lizano N <leoln123@live.com>
2024-12-15 17:13:05 -0600

    Added missions UI for basic player guidance

--- GameJamPlus2024/VRIF3/Assets/Scripts/MissionManager.cs
+++ GameJamPlus2024/VRIF3/Assets/Scripts/MissionManager.cs
@@ -1,8 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MissionManager : MonoBehaviour
 {
     [SerializeField] private DeliverBoxBehavior deliverBox;
+
+    [SerializeField] private Image flowerImage;
+    [SerializeField] private Sprite flowerSprite;
+    [SerializeField] private TMP_Text missionText;
+    [SerializeField] private GameObject secondImageGameObject;
 
     public AudioClip finishedDemo;
 
@@ -17,10 +24,14 @@ public class MissionManager : MonoBehaviour
                 //Oh God I hate my self for this, but I gotta run!
                 if(finishedDemo != null)
                     AudioSource.PlayClipAtPoint(finishedDemo, deliverBox.transform.position);
+                missionText.text = "Thank you sweetheart! See you tomorrow!";
                 return;
             }
 
             deliverBox.UpdateDesiredFlower(SeedBehavior.SeedType.Flower);
+            flowerImage.sprite = flowerSprite;
+            missionText.text = "Lovely! Now I need a pine flower!";
+            secondImageGameObject.SetActive(true);
             secondMission = true;
             
         }
