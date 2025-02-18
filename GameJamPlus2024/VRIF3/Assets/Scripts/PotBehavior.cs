859f019c294b7e585fa94429a5f2667eb4b16d7e
Leonardo Lizano N <leoln123@live.com>
2025-02-18 16:08:15 -0600

    Removed leftover code looking forward to save system

--- GameJamPlus2024/VRIF3/Assets/Scripts/PotBehavior.cs
+++ GameJamPlus2024/VRIF3/Assets/Scripts/PotBehavior.cs
@@ -8,10 +8,7 @@ public class PotBehavior : MonoBehaviour
     [SerializeField] MeshFilter potFlowerMesh;
     [SerializeField] SeedBehavior.SeedType desiredSeed;
     [SerializeField] GameObject flowerVisuals;
-    //[SerializeField] GameObject[] flowerVisuals;
     [SerializeField] GameObject grabbableFlower;
-    //[SerializeField] GameObject grabbableCala;
-    //[SerializeField] GameObject grabbableHeliconia;
     [SerializeField] float spawnCooldown = 10f;
 
     private bool hasBeenPlanted = false;
@@ -47,20 +44,6 @@ public class PotBehavior : MonoBehaviour
             currentCooldown = 0;
             flower.SetActive(true);
             flower.GetComponent<Animator>().Play("Flower");
-            //switch (seedType)
-            //{
-            //    case SeedBehavior.SeedType.Flower:
-            //        flowerVisuals[0].SetActive(true);
-            //        break;
-
-            //    case SeedBehavior.SeedType.Heliconia:
-            //        flowerVisuals[1].SetActive(true);
-            //        break;
-
-            //    case SeedBehavior.SeedType.Cala:
-            //        flowerVisuals[2].SetActive(true);
-            //        break;
-            //}
             StartCoroutine(SpawnGrabbableFlower());
         }
     }
@@ -73,29 +56,5 @@ public class PotBehavior : MonoBehaviour
         instFlower.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         instFlower.transform.SetParent(transform, true);
         instFlower.SetActive(true);
-        //switch(seedType)
-        //{
-        //    case SeedBehavior.SeedType.Flower:
-        //        var instFlower = Instantiate(grabbableFlower, grabbableFlower.transform.position, grabbableFlower.transform.rotation);
-        //        instFlower.transform.SetParent(transform, true);
-        //        instFlower.SetActive(true);
-        //        //grabbableFlower.SetActive(true);
-        //        break;
-
-        //    case SeedBehavior.SeedType.Heliconia:
-        //        var instFlower2 = Instantiate(grabbableHeliconia, grabbableHeliconia.transform.position, grabbableHeliconia.transform.rotation);
-        //        instFlower2.SetActive(true);
-        //        instFlower2.transform.SetParent(transform, true);
-        //        //grabbableHeliconia.SetActive(true);
-        //        break;
-
-        //    case SeedBehavior.SeedType.Cala:
-        //        var instFlower3 = Instantiate(grabbableCala, grabbableCala.transform.position, grabbableCala.transform.rotation);
-        //        instFlower3.SetActive(true);
-        //        instFlower3.transform.SetParent(transform, true);
-        //        //grabbableCala.SetActive(true);
-        //        break;
-        //}
-
     }
 }
