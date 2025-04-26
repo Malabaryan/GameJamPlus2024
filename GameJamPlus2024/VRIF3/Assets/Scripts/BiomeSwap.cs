30910d9d01bdcc02215dfc2004d3242ff85e05c4
Leonardo Lizano N <leoln123@live.com>
2025-04-26 16:21:56 -0600

    Placed swap biome asset in World scene for transition to snow biome

--- GameJamPlus2024/VRIF3/Assets/Scripts/BiomeSwap.cs
+++ GameJamPlus2024/VRIF3/Assets/Scripts/BiomeSwap.cs
@@ -1,4 +1,6 @@
+using FlatKit;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BiomeSwap : MonoBehaviour
 {
@@ -7,23 +9,53 @@ public class BiomeSwap : MonoBehaviour
     [SerializeField] AudioClip newBiomeTrack;
 
     [Header("Visuals Settings")]
-    [SerializeField] AudioSource newBiomeVisuals;
+    [SerializeField] FlatKitFog FogSettings;
+    [SerializeField] FogSettings oldFog;
+    [SerializeField] FogSettings newFog;
 
-    private bool doSwap = false;
+    private float oldFogDistanceFogIntensity = 0f;
+    private float oldFogHeightFogIntensity = 0f;
+    private float newdFogDistanceFogIntensity = 0f;
+    private float newFogHeightFogIntensity = 0f;
+
+    private bool doSwapMusic = false;
     private bool volumeDown = true;
+
+    private bool doSwapFog = false;
+    private bool fogDown = true;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (audioSource.clip == newBiomeTrack) return;
         Debug.Log("SWITCH");
         //If ship collides with this object make a transition animation
-        doSwap = true;
+        doSwapMusic = true;
         volumeDown = true;
+
+        //Swap biome
+        FogSettings.settings = newFog;
+        FogSettings.settings.useDistance = false;
+        FogSettings.settings.useHeight = false;
+        FogSettings.settings.useDistance = true;
+        FogSettings.settings.useHeight = true;
+        //doSwapFog = true;
+        //fogDown = true;
+        //oldFog = FogSettings.settings;
+        //oldFogDistanceFogIntensity = oldFog.distanceFogIntensity;
+        //oldFogHeightFogIntensity = oldFog.heightFogIntensity;
+        //newdFogDistanceFogIntensity = newFog.distanceFogIntensity;
+        //newFogHeightFogIntensity = newFog.heightFogIntensity;
     }
 
     private void Update()
     {
         //Swap music
-        if(doSwap)
+        if(doSwapMusic)
         {
             //Volume down and swap music
             if(volumeDown)
@@ -31,7 +63,7 @@ public class BiomeSwap : MonoBehaviour
             else if(volumeDown == false && audioSource.volume < 0.5f)
                 audioSource.volume += Time.deltaTime * 0.2f;
             else
-                doSwap = false; //Should do the swap when clip has changed
+                doSwapMusic = false; //Should do the swap when clip has changed
 
             if (audioSource.volume <= 0) 
             {
@@ -40,5 +72,35 @@ public class BiomeSwap : MonoBehaviour
                 volumeDown = false;
             }
         }
+
+        //Swap biome smooth transition
+        //Si funciona pero no se nota ingame
+        //if(doSwapFog)
+        //{
+        //    if(fogDown)
+        //    {
+        //        //Intensity down
+        //        if(oldFog.distanceFogIntensity > 0f) oldFog.distanceFogIntensity -= Time.deltaTime * 0.1f;
+        //        if (oldFog.heightFogIntensity > 0f) oldFog.heightFogIntensity -= Time.deltaTime * 0.1f;
+        //        if (oldFog.distanceFogIntensity <= 0 && oldFog.heightFogIntensity <= 0)
+        //        {
+        //            //Do the magic swap current fog lowered intensity, set new fog intensity to 0 to start going up
+        //            newFog.distanceFogIntensity = 0f;
+        //            newFog.heightFogIntensity = 0f;
+        //            FogSettings.settings = newFog;
+        //            oldFog.distanceFogIntensity = oldFogDistanceFogIntensity;
+        //            oldFog.heightFogIntensity = oldFogHeightFogIntensity;
+        //            fogDown = false;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //Intensity up
+        //        if (newFog.distanceFogIntensity < newdFogDistanceFogIntensity) newFog.distanceFogIntensity += Time.deltaTime * 0.1f;
+        //        if (newFog.heightFogIntensity < newFogHeightFogIntensity) newFog.heightFogIntensity += Time.deltaTime * 0.1f;
+        //        if (newFog.distanceFogIntensity >= newdFogDistanceFogIntensity && 
+        //            newFog.heightFogIntensity >= newFogHeightFogIntensity) doSwapFog = false;
+        //    }
+        //}
     }
 }
