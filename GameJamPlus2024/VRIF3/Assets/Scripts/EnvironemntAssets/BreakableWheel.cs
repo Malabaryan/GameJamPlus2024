aa1e0d356dfdb572d0b759e0f9628e4398289343
Leonardo Lizano N <leoln123@live.com>
2025-08-11 18:20:42 -0600

    Added confetti explosion when hitting three rotating targets

---
+++ GameJamPlus2024/VRIF3/Assets/Scripts/EnvironemntAssets/BreakableWheel.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BreakableWheel : MonoBehaviour
+{
+    public GameObject confettiPrefab;
+
+    [SerializeField] private bool[] destroyedTargets;
+
+    public void DestroyTarget(int index)
+    {
+        destroyedTargets[index] = true;
+        checkAllTargets();
+    }
+
+    public void RespawnTarget(int index)
+    {
+        destroyedTargets[index] = false;
+        checkAllTargets();
+    }
+
+    private void checkAllTargets()
+    {
+        for (int i = 0; i < destroyedTargets.Length; i++)
+        {
+            if (destroyedTargets[i] == false) return;
+        }
+        //if it gets here, all targets were destroyed
+        GameObject confetti = Instantiate(confettiPrefab, transform.position, Quaternion.identity);
+        Destroy(confetti, 5f);
+    }
+}
